de94d8b2162957d22542f259731d5568fc4817cf
RoOboOcoOp <wuldsonfernandes@gmail.com>
2017-12-03 18:26:38 -0200

    Pilha pronta msm

--- src/br/ufop/controller/PilhaExpressoes.java
+++ src/br/ufop/controller/PilhaExpressoes.java
@@ -14,36 +14,95 @@ package br.ufop.controller;
  * @author UFOP
  */
 public class PilhaExpressoes{
-    private int size;
     private String[] elementos;
+    private int size;
+    
     //private final List<Tag> tag = new LinkedList<>();
     //PilhaTag pilha = new PilhaTag();
    
-    /**
-     * @return the size
-     */
-    public int getSize() {
-        return size;
+    public PilhaExpressoes(int capacity) {  
+	/* 
+	* Utilizamos o método abs da classe Math só pra evitar que se tente criar uma pilha "devedora"... 
+	*/  
+            elementos = new String[Math.abs(capacity)];  
+            size = 0;  
+	}
+    
+    public String push(String elemento) {  
+	if(elemento == null)  
+            throw new IllegalArgumentException("O elemento não pode ser nulo!");  
+        if(size == elementos.length)  
+            return null;  
+	size++;  
+	elementos[getTopPosition()] = elemento;  
+	return elemento;  
     }
-
-    /**
-     * @param size the size to set
-     */
-    public void setSize(int size) {
-        this.size = size;
+    
+    public String peek() {  
+	if(isEmpty())  
+            return null;  
+	return elementos[getTopPosition()];  
     }
-
-    /**
-     * @return the elementos
-     */
-    public String[] getElementos() {
-        return elementos;
+    
+    public String pop() {
+	String result = peek();  
+	/*Se havia um elemento no topo da pilha...*/  
+	if(result != null) {  
+            elementos[getTopPosition()] = null;  
+            size--;  
+	}  
+	return result;  
     }
-
+    
+    /** 
+     * Método utilizado para limpar todo o conteúdo da pilha. 
+     */  
+    public void clear() {  
+        for(int i = 0; i < size; i++){  
+            elementos[i] = null;
+        }
+        size = 0;  
+    }  
     /**
-     * @param elementos the elementos to set
-     */
-    public void setElementos(String[] elementos) {
-        this.elementos = elementos;
+     * Método utilizado para se obter o tamanho (número de elementos) da pilha 
+     * @return 
+     */  
+    public int getSize() {  
+        return size;  
+    }  
+    /** 
+     * Método utilizado para se obter a capacidade da pilha 
+     * @return 
+     */  
+    public int getCapacity() {  
+        return elementos.length;  
     }
+    /**
+     * Método utilizado para verificar se a pilha está vazia. Se for o caso, 
+     * será retornado true, caso contrário, será retornado false.
+     * @return 
+     */
+    public boolean isEmpty() {
+        return size <= 0;
+    }
+    
+    private int getTopPosition() {
+        if (isEmpty())
+            return 0;
+        return size - 1;  
+    }
+    /** 
+     * Este método serve para representar textualmente esta pilha 
+     * @see java.lang.Object#toString() 
+     */  
+    public String toString() {  
+        StringBuilder sb = new StringBuilder("[");  
+	for(int i = 0; i < size; i++) {  
+            sb.append(elementos[i]);  
+            if(i < size - 1)  
+                sb.append(" | ");  
+	}  
+	sb.append(">");  
+	return sb.toString();  
+    }  
 }
